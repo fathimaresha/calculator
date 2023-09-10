31af07109f90005c49d7528e4a54b6c7b344df2f
resha fathima <fathimaresha>
2023-09-10 22:21:01 +0530

    calculator

---
+++ index.js
@@ -0,0 +1,20 @@
+//1 display number in the box
+function displayNum(num) {
+    result.value+=num
+}
+
+
+//2 clear the  box
+function clearNum(){
+    result.value=''
+}
+
+
+
+//3 evaluate expretion
+function evaluateNum(){
+    result.value=eval(result.value)
+}
+
+
+//4 remove last item from the text box
